17b2d1218a31ed5a1fe54c02b487581655d781c9
[Cloud2] 지준혁 <jinyhehe@gmail.com>
2024-04-08 16:14:19 +0000

    fix: 구글 인증 주소 수정

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,7 +8,7 @@ const onNaverLogin = () => {
 };
 
 const onGoogleLogin = () => {
-  window.location.href = "http://localhost:8080/oauth2/authorization/naver";
+  window.location.href = "http://localhost:8080/login/oauth2/code/google";
 };
 
 export default function SignIn() {
