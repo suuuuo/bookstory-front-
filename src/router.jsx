1398f8f5e15d2031c79a9b06780d91aa1269d5c9
soo <yoons1609@gmail.com>
2024-04-11 01:18:44 +0900

    feat: router 수정

--- src/router.jsx
+++ src/router.jsx
@@ -1,64 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Books from "./pages/Cart/Samplebook/books";
-import Book1 from "./pages/Cart/Samplebook/sample_book1";
-import Book2 from "./pages/Cart/Samplebook/sample_book2";
-import Book3 from "./pages/Cart/Samplebook/sample_book3";
-import Book4 from "./pages/Cart/Samplebook/sample_book4";
 import Cart from "./pages/Cart/cart";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Books />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/Book1",
-    element: (
-      <>
-        <Header />
-        <Book1 />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/Book2",
-    element: (
-      <>
-        <Header />
-        <Book2 />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/Book3",
-    element: (
-      <>
-        <Header />
-        <Book3 />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/Book4",
-    element: (
-      <>
-        <Header />
-        <Book4 />
-        <Footer />
-      </>
-    ),
-  },
   {
     path: "/Cart",
     element: (
