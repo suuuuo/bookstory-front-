6bbc709f49cb675d2b6850ab4b3fd0162f123507
Jong-Min <codejomo99@gmail.com>
2024-04-12 12:01:47 +0900

    feat: 북메인  CSS

--- src/pages/bookdetail/Productpage.jsx
+++ src/pages/bookdetail/Productpage.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import BookDetail from "./BookDetail.jsx"; // 상품 정보 컴포넌트
-import Qna from "./Qna.jsx"; // Q&A 컴포넌트
+import BookDetail from "./BookDetail.jsx";
+import Qna from "./Qna.jsx";
+import "../../css/ProductPage.css"; // CSS 파일 임포트
 
 export default function Productpage() {
-  const [activeSection, setActiveSection] = useState("bookDetail"); // 활성 섹션을 제어하는 상태
+    const [activeSection, setActiveSection] = useState("bookDetail");
 
-  return (
-    <div>
-      <button onClick={() => setActiveSection("bookDetail")}>상품 정보</button>
-      <button onClick={() => setActiveSection("qna")}>Q&A</button>
-      {activeSection === "bookDetail" && <BookDetail />}
-      {activeSection === "qna" && <Qna />}
-    </div>
-  );
+    return (
+        <div className="productPage-container">
+            <div className="textContainer">
+                <span className="clickableText" onClick={() => setActiveSection("bookDetail")}>상품 정보</span>
+                <span className="clickableText" onClick={() => setActiveSection("qna")}>Q&A</span>
+
+            </div>
+            <div className="divider"></div>
+            {activeSection === "bookDetail" && <BookDetail />}
+            {activeSection === "qna" && <Qna />}
+
+        </div>
+
+    );
 }
