c396a575af60e776cfbb5e219d80bc2a6e76150a
haileyeong <haileyeong@gmail.com>
2024-04-15 20:17:15 +0900

    build: 포맷 설정

--- src/router.jsx
+++ src/router.jsx
@@ -2,20 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Test from "./pages/Test";
 import SignIn from "./pages/SignIn";
 import SocialLoginHandler from "./pages/SocialLoginHandler";
-import Header from "./Components/Header.jsx"
+import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-import Cart from "./pages/cart/cart";
-import Main from "./pages/bookdetail/main.jsx";
-import QnA from "./pages/bookdetail/qna.jsx";
-import ProductPage from "./pages/bookdetail/productpage.jsx";
-import OrderList from "./pages/order/orderList.jsx";
-=======
 import Cart from "./pages/cart";
 import Main from "./pages/bookdetail/Main.jsx";
 import Productpage from "./pages/bookdetail/Productpage.jsx";
->>>>>>> aa1bf74c6ac52cc74517bb1bf344940983f10e0a
+import OrderList from "./pages/order/orderList.jsx";
 
 const router = createBrowserRouter([
   {
