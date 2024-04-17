296db23ca0e2171bf5ec7f145e74bd8619bdc5b4
haileyeong <haileyeong@gmail.com>
2024-04-18 01:12:24 +0900

    feat: Header에 mypage 연결

--- src/router.jsx
+++ src/router.jsx
@@ -11,6 +11,9 @@ import BookCategory from "./pages/BookCategory.jsx";
 import BookMain from "./pages/BookMain.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import BookSave from "./pages/BookSave.jsx";
+import OrderList from "./pages/order/getAllMyOrders.jsx";
+import MyPage from "./pages/MyPage.jsx";
+import SaveOrder from "./pages/order/saveOrder.jsx";
 
 const router = createBrowserRouter([
   {
